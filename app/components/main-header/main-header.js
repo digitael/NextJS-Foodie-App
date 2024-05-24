e2f46fa17780987c1273670b05d8165dcd806f50
Christian van Lierop <chrisvanlierop@gmail.com>
2024-05-24 23:10:49 +0200

    Finished lesson 106

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -4,6 +4,7 @@ import MainHeaderBackground from './main-header-background';
 
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
   return (
@@ -18,14 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link className='navLink' href={'./meals'}>
-                Browse Meals
-              </Link>
+              <NavLink href={'/meals'}>Browse Meals</NavLink>
             </li>
             <li>
-              <Link className='navLink' href={'./community'}>
-                Foodies Community
-              </Link>
+              <NavLink href={'/community'}>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
